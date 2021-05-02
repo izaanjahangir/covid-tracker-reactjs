f36c53c9c8bc1e495bd9209c1fbf0ee7a06882bf
izaanjahangir <izaanjahangir2@gmail.com>
2021-05-02 21:36:57 +0500

    small UI improvements

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -3,7 +3,7 @@ function Background(props) {
     <div className="h-full flex flex-col">
       <div className="h-full w-full flex flex-col fixed top-0 left-0">
         <div className="h-16 bg-purple-700"></div>
-        <div className="bg-white flex-1"></div>
+        <div className="bg-gray-100 flex-1"></div>
       </div>
       <div className="z-10">{props.children}</div>
     </div>
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -9,7 +9,7 @@ function Home() {
         <div className="mx-auto max-w-5xl">
           <StatsOverview />
           <div className="mt-2 flex">
-            <div className="flex-1 bg-white p-2 shadow-2xl border border-grey">
+            <div className="mx-4 sm:mx-0 rounded-md flex-1 bg-white p-2 shadow-2xl border border-gray-300">
               <LineGraph />
             </div>
           </div>
